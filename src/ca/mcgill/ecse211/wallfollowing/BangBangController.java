4bd318f0d13346e4d2dfeca9082f84ccc98da97d
unknown <eric.huie@mail.mcgill.ca>
2017-09-12 15:09:42 -0400

    Bang Bang Controller Edit 1

--- src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -4,32 +4,55 @@ import lejos.hardware.motor.*;
 
 public class BangBangController implements UltrasonicController {
 
-  private final int bandCenter;
-  private final int bandwidth;
-  private final int motorLow;
-  private final int motorHigh;
-  private int distance;
+	private final int bandCenter;
+	private final int bandwidth;
+	private final int motorLow;
+	private final int motorHigh;
+	private int distance;
 
-  public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
-    // Default Constructor
-    this.bandCenter = bandCenter;
-    this.bandwidth = bandwidth;
-    this.motorLow = motorLow;
-    this.motorHigh = motorHigh;
-    WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
-    WallFollowingLab.rightMotor.setSpeed(motorHigh);
-    WallFollowingLab.leftMotor.forward();
-    WallFollowingLab.rightMotor.forward();
-  }
+	public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
+		// Default Constructor
+		this.bandCenter = bandCenter;
+		this.bandwidth = bandwidth;
+		this.motorLow = motorLow;
+		this.motorHigh = motorHigh;
+		WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
+		WallFollowingLab.rightMotor.setSpeed(motorHigh);
+		WallFollowingLab.leftMotor.forward();
+		WallFollowingLab.rightMotor.forward();
+	}
 
-  @Override
-  public void processUSData(int distance) {
-    this.distance = distance;
-    // TODO: process a movement based on the us distance passed in (BANG-BANG style)
-  }
+	@Override
+	public void processUSData(int distance) {
+		this.distance = distance;
+		// TODO: process a movement based on the us distance passed in (BANG-BANG style)
+		//boolean end = true;
+		
+		int distError = bandCenter - distance;
+			if(Math.abs(distError) <= bandwidth) {
+				WallFollowingLab.leftMotor.setSpeed(motorHigh);
+				WallFollowingLab.rightMotor.setSpeed(motorHigh);
+				WallFollowingLab.leftMotor.forward();
+				WallFollowingLab.rightMotor.forward();
+			}
+			else if(distError > 0) {
+				WallFollowingLab.leftMotor.setSpeed(motorHigh-motorLow);
+				WallFollowingLab.rightMotor.setSpeed(motorHigh+motorLow);
+				WallFollowingLab.leftMotor.forward();
+				WallFollowingLab.rightMotor.forward();
+			}
+			else{
+				WallFollowingLab.leftMotor.setSpeed(motorHigh+motorLow);
+				WallFollowingLab.rightMotor.setSpeed(motorHigh-motorLow);
+				WallFollowingLab.leftMotor.forward();
+				WallFollowingLab.rightMotor.forward();
+			}
+		
 
-  @Override
-  public int readUSDistance() {
-    return this.distance;
-  }
+	}
+
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+	}
 }
