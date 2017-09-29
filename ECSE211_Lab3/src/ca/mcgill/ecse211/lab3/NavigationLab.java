2cdb5e062f078a9ce5f5d83089f5b450a06ae47d
unknown <eric.huie@mail.mcgill.ca>
2017-09-29 17:07:39 -0400

    Changes to Navigation

--- ECSE211_Lab3/src/ca/mcgill/ecse211/lab3/NavigationLab.java
+++ ECSE211_Lab3/src/ca/mcgill/ecse211/lab3/NavigationLab.java
@@ -2,9 +2,14 @@ package ca.mcgill.ecse211.lab3;
 
 public class NavigationLab {
 
+	private static int[][] coordinates = { {2,1}, {1,1}, {1,2}, {2,0} };
+	
+	private static Navigation controller = new Navigation(0,0);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		controller.travelTo(2,1);
+		
 	}
 
 }
